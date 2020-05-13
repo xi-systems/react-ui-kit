6ffa5cc3a8340b3bb09e321ba163250517caa218
ReneMcCaine <rene@mccaine.me>
2020-05-13 07:01:01 +0100

    Add avatar, badge

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 import Button from "./components/Button";
 import Icon from "./components/Icon";
 import LoaderIndicator from "./components/LoaderIndicator";
+import Badge from './components/Badge'
+import Avatar from './components/Avatar'
 
 //Shared
 import { icons } from "./shared/icons";
-export { Button, Icon, LoaderIndicator, icons };
+export { Button, Icon, LoaderIndicator, Badge, Avatar, icons };
